d3284bffe4e231f452f6aa56bea28456aa27054e
koloinarajaofera <koloina.rajaofera@gmail.com>
2026-08-20 06:26:01 +0300

    Refactor QR code scanning script

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
     var isBusy = false; // vrai pendant le traitement d'un scan (évite les doublons)
     var isRunning = false; // vrai tant que la caméra scanne activement
     var selectedMode = 'entree';
-    var API_URL = (typeof window !== 'undefined' && window.API_URL) || 'https://script.google.com/macros/s/AKfycbwJxQgDskYORczxY-HW_tTmHOL4M0QRVfoG5qmryzi6cxUhEwX4oE-G8gNg-SVYONc/exec';
+    var API_URL = (typeof window !== 'undefined' && window.API_URL) || 'https://script.google.com/macros/s/AKfycbx3-zb3HF1gFVXpLyWJL6rdQySSq0-6eX_-LX1E_u7aR1eyhsJxAJewXyn6hYfZsQ4/exec';
     var config = { fps: 12, qrbox: { width: 250, height: 250 } };
 
     function setMode(mode) {
